57e524449f40311c9bc4e9b02064df42a33eb3c0
enricMercura <emv@mercura.dk>
2024-12-05 18:38:31 +0100

    fix import

--- esplai-can-cristu-vite/src/Pages/Ideari.tsx
+++ esplai-can-cristu-vite/src/Pages/Ideari.tsx
@@ -1,14 +1,15 @@
-import React from "react"
-import Accordion from "../Components/accordion"
+import React from 'react';
+import Accordion from '../Components/Accordion';
 
 export const Ideari = () => {
-
-    return (
-        <div className="size-full overflow-y-auto text-center">
-            <h1 className="text-5xl desktop:text-6xl pt-20 pb-16 text-white">Ideari</h1>
-            <div className=" px-12 py-6 laptop:px-32  laptop:py-16">
-                <Accordion />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="size-full overflow-y-auto text-center">
+      <h1 className="text-5xl desktop:text-6xl pt-20 pb-16 text-white">
+        Ideari
+      </h1>
+      <div className=" px-12 py-6 laptop:px-32  laptop:py-16">
+        <Accordion />
+      </div>
+    </div>
+  );
+};
