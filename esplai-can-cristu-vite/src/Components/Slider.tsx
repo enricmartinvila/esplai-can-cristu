6eb822d83797b51b5d65a26e99deb06007f56a1a
enricMercura <emv@mercura.dk>
2024-12-11 16:40:44 +0100

    fix images comprimize and file for img urls constants

--- esplai-can-cristu-vite/src/Components/Slider.tsx
+++ esplai-can-cristu-vite/src/Components/Slider.tsx
@@ -1,53 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
+import { imagesUrls } from '../constants/SliderConstants';
 
 export default function Slider() {
-  const images = [
-    'images/img1.webp',
-    'images/img2.webp',
-    'images/img3.webp',
-    'images/img4.webp',
-    'images/img5.webp',
-    'images/img6.webp',
-    'images/img7.webp',
-    'images/img8.webp',
-    'images/img9.webp',
-    'images/img10.webp',
-    'images/img11.webp',
-    'images/img12.jpg',
-    'images/img13.webp',
-    'images/img14.webp',
-    'images/img15.webp',
-    'images/img16.webp',
-    'images/img17.jpg',
-    'images/img18.jpg',
-    'images/img19.jpg',
-    'images/img20.jpg',
-    'images/img21.jpg',
-  ];
+ 
   const [currentImage, setCurrentImage] = useState(0);
 
   const handlePrevClick = () => {
     setCurrentImage((prevImage) =>
-      prevImage === 0 ? images.length - 1 : prevImage - 1
+      prevImage === 0 ? imagesUrls.length - 1 : prevImage - 1
     );
   };
 
   const handleNextClick = () => {
     setCurrentImage((prevImage) =>
-      prevImage === images.length - 1 ? 0 : prevImage + 1
+      prevImage === imagesUrls.length - 1 ? 0 : prevImage + 1
     );
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImage((prevImage) =>
-        prevImage === images.length - 1 ? 0 : prevImage + 1
+        prevImage === imagesUrls.length - 1 ? 0 : prevImage + 1
       );
     }, 5000);
 
     return () => clearInterval(interval);
-  }, [images.length]);
+  }, [imagesUrls.length]);
 
   return (
     <div className="relative w-full h-full">
@@ -59,7 +38,7 @@ export default function Slider() {
           <ChevronLeftIcon className="size-5" />
         </button>
         <img
-          src={images[currentImage]}
+          src={imagesUrls[currentImage]}
           alt={`Slide ${currentImage}`}
           className="w-full h-full object-cover"
         />
